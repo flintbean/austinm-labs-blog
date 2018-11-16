6e60c47088cf315289887b6d1094c2c1d4f85e01
flintbean <ajmerando1@yahoo.com>
2018-11-16 11:15:22 -0800

    first changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'Labs 8 Blog',
+    author: 'Austin Merando',
+    description: 'A blog that follows my day to day with Lambda schools labs',
+    siteUrl: 'https://austinm-labs-blog.netlify.com/',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
